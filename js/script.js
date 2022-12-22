8367621a90a752fd0e460324b9abbfc1880a0983
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-22 13:43:32 +0500

    add

--- js/script.js
+++ js/script.js
@@ -33,3 +33,24 @@ document.querySelector('#cart-btn').onclick = () =>{
 document.querySelector('#close-cart').onclick = () =>{
    cart.classList.remove('active');
 }
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   myOrders.classList.remove('active');
+   cart.classList.remove('active');
+};
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next(){
+   slides[index].classList.remove('active');
+   index = (index + 1) % slides.length;
+   slides[index].classList.add('active');
+}
+
+function prev(){
+   slides[index].classList.remove('active');
+   index = (index - 1 + slides.length) % slides.length;
+   slides[index].classList.add('active');
+}
