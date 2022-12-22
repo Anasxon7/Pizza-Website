b05390aff99725940da7e7518c3f5b115f7c6266
Madaminov <muhammadanasxon6694@gmail.com>
2022-12-22 13:42:47 +0500

    add

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ let navbar = document.querySelector('.header .flex .navbar');
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
 }
+
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () =>{
+   account.classList.add('active');
+}
+
+document.querySelector('#close-account').onclick = () =>{
+   account.classList.remove('active');
+}
+
+let myOrders = document.querySelector('.my-orders');
+
+document.querySelector('#order-btn').onclick = () =>{
+   myOrders.classList.add('active');
+}
+
+document.querySelector('#close-orders').onclick = () =>{
+   myOrders.classList.remove('active');
+}
